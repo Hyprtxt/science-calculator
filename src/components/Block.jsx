99d6cccc7f6c07c5f146d2d0580beb718cb45404
Taylor Y <taylor@cruzscience.com>
2019-08-16 10:15:01 -0700

    Block rewrite with useResizeRect hook

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ButtonBase from '@material-ui/core/ButtonBase';
+import useResizeRect from '../hooks/useResizeRect';
 
-const Item = ({
-  children,
-  onClick,
-  active,
-  enabled,
-  className,
-  buttonRef,
-  style
-}) => {
+const Block = ({ children, onClickItem, active, enabled, className }) => {
+  const buttonRef = React.useRef(null);
+  const rect = useResizeRect(buttonRef);
+  let blockTextStyle = { fontSize: '18px', lineHeight: '22px' };
+  if (rect.width < 150) {
+    blockTextStyle.fontSize = '14px';
+    blockTextStyle.lineHeight = '18px';
+  }
+  if (rect.width < 100) {
+    blockTextStyle.fontSize = '10px';
+    blockTextStyle.lineHeight = '14px';
+  }
   let extraClassName = className;
   if (active) {
     extraClassName += ' active';
@@ -22,10 +26,14 @@ const Item = ({
     : { disableRipple: true };
   return (
     <ButtonBase
-      style={style}
-      buttonRef={buttonRef}
       className={`${extraClassName}`}
-      onClick={onClick}
+      style={blockTextStyle}
+      buttonRef={buttonRef}
+      onClick={e => {
+        if (enabled) {
+          onClickItem(e);
+        }
+      }}
       {...disableRipple}
     >
       {children}
@@ -33,41 +41,4 @@ const Item = ({
   );
 };
 
-const Block = ({ children, name, enabled, active, onClickItem, className }) => {
-  const [width, setWidth] = React.useState(0);
-  let blockTextStyle = { fontSize: '18px', lineHeight: '22px' };
-  const styleRef = React.useCallback(node => {
-    if (node !== null) {
-      // console.log(node.children[0].children[0].scrollHeight);
-      // console.log(node.getBoundingClientRect(), window.devicePixelRatio);
-      setWidth(node.getBoundingClientRect().width);
-    }
-  }, []);
-  if (width < 150) {
-    blockTextStyle.fontSize = '14px';
-    blockTextStyle.lineHeight = '18px';
-  }
-  if (width < 100) {
-    blockTextStyle.fontSize = '10px';
-    blockTextStyle.lineHeight = '14px';
-  }
-  // console.log(width, width < 100, blockTextStyle);
-  return (
-    <Item
-      buttonRef={styleRef}
-      style={blockTextStyle}
-      onClick={e => {
-        if (enabled) {
-          onClickItem(e);
-        }
-      }}
-      name={name}
-      active={active}
-      enabled={enabled}
-      className={className}
-    >
-      {children}
-    </Item>
-  );
-};
 export default Block;
